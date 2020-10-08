8b2c4c2073de796abe7c335cbba5b9aed572dace
Ali Raza <hassanrazaalvi217@gmail.com>
2020-10-08 11:40:32 +0500

    updated

--- src/components/CountryData.js
+++ src/components/CountryData.js
@@ -1,7 +1,6 @@
 import React,{useEffect,useState} from 'react';
 import { makeStyles } from '@material-ui/core/styles';
-import Paper from '@material-ui/core/Paper';
-import Grid from '@material-ui/core/Grid';
+
 import AOS from 'aos';
 import 'aos/dist/aos.css';
 
